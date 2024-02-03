23d82028b8286f7ba45d23de58a923a0af1f6bdb
fbhood <mail@fabiopacifici.com>
2024-02-03 11:27:50 +0100

    add comic card

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: 'ComicCard',
+  props: {
+    thumb: String,
+    title: String
+  }
+}
+</script>
+<template>
+  <div class="card">
+    <img :src="thumb" :alt="title + 'image'" />
+    <h5>{{ title }}</h5>
+  </div>
+</template>
+
+<style scoped>
+.card
+{
+  & > img
+  {
+    aspect-ratio: 1;
+    object-fit: cover;
+    object-position: top;
+  }
+}
+</style>
